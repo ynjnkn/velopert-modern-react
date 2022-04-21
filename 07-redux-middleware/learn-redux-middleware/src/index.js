03aa3c0feedfd68181ce08a73c5b36cfcc64eaef
ynjnkn <yjun_k@hotmail.com>
2022-04-21 17:11:59 +0900

    7. 리덕스 미들웨어_2. 미들웨어 만들어보고 이해하기 [완료]

--- 07-redux-middleware/learn-redux-middleware/src/index.js
+++ 07-redux-middleware/learn-redux-middleware/src/index.js
@@ -2,11 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
 import { Provider } from "react-redux";
 import rootReducer from "./redux/modules";
+import myLogger from "./middlewares/myLogger";
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(myLogger));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
---
+++ 07-redux-middleware/learn-redux-middleware/src/middlewares/myLogger.js
@@ -0,0 +1,11 @@
+const myLogger = (store) => (next) => (action) => {
+  console.log(action); // 액션 출력
+  const result = next(action); // 다음 미들웨어(또는 리듀서)에게 액션 전달
+
+  // 업데이트 이후의 상태 조회
+  console.log(store.getState());
+
+  return result; // 여기서 반환하는 값은 dispatch(action)의 결과물
+};
+
+export default myLogger;
